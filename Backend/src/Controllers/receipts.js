323dca297c0328e40bc4a759c5d9af0c5e10a3ea
Wilfred Muchire <logincoin40@gmail.com>
2024-09-05 11:27:51 +0300

    adding functinality to receipt http requests

--- Backend/src/Controllers/receipts.js
+++ Backend/src/Controllers/receipts.js
@@ -2,26 +2,61 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient()
 
 export const getAllReceipts = async(req, res) => {
-    res.send('Get all the receipts.')
+   try {
+     const getReceipts = await prisma.receipts.findMany({
+        select: {
+            receipt_id: true,
+            receiptNumber: true,
+            amount_paid: true,
+            deadline_date: true,
+            customer: true,
+            meters: true,
+            billing:true
+
+        }
+     })
+
+     if (getReceipts != null){
+        res.status(200).json({success:true, message: 'All receipts found successfully.', data: getReceipts})
+     } else{
+        res.status(500).json({success:true, message: 'Something went wrong. Server error'})
+     }
+   } catch (error) {
+    res.status(500).json({success: false, message: error.message})
+   }
 }
 
 export const getSingleReceipt = async(req, res) => {
-    res.send('Get single receipts.')
+   try {
+    const {receipt_id} = req.params;
+
+    const getReceipt = await prisma.receipts.findUnique({
+        where: {receipt_id:receipt_id}
+    })
+
+     if (getReceipt != null){
+        res.status(200).json({success:true, message: 'Receipt found successfully.', data: getReceipt})
+     } else{
+        res.status(500).json({success:false, message: 'Something went wrong. Server error'})
+     }
+   } catch (error) {
+    res.status(500).json({success:false, message:error.message})
+   }
 }
 
 export const generateReceipt = async(req, res) => {
     try {
-        const {receiptNumber, amount_paid, Deadline_date, bill_id,meter_id, cust_id} = req.body;
+        const {receiptNumber, amount_paid, deadline_date, bill_id,meter_id, cust_id} = req.body;
 
-        let custId = 'f3583c7d-3d74-49fe-9cbd-6f22e6ad5ac2'
-        let meterID = '3d14658c-659f-4379-a103-5a78549832f4'
-        let billID = 'c0304240-e78f-4ce8-abcc-b83a4ef6ce8e'
+        let custId = '5da90084-ab2a-4b74-af15-537fd6d910e3'
+        let meterID = 'c46ab4e5-92fd-4b60-9a47-4c5b1a8369d1'
+        let billID = 'a8d665e2-2464-4c7e-97f7-f2b2627f9201'
 
         const generate = await prisma.receipts.create({
             data:{
                 receiptNumber,
                 amount_paid, 
-                Deadline_date,
+                deadline_date,
                 billing:{
                     connect:{
                         bill_id:billID
@@ -37,12 +72,11 @@ export const generateReceipt = async(req, res) => {
                         cust_id:custId
                     }
                 }
-                
             }
         })
 
         if (generate != null){
-            res.status(200).json({success: true, message:"Bill generated successfully."})
+            res.status(200).json({success: true, message:"Receipt generated successfully."})
         } else{
             res.status(500).json({success: false, message:"Server error."})
 
@@ -54,5 +88,22 @@ export const generateReceipt = async(req, res) => {
 }
 
 export const deleteReceipt =  async(req, res) => {
-    res.send('Delete receipt.')
+    try {
+    const {receipt_id} = req.params;
+
+    const getReceipt = await prisma.receipts.findUnique({
+        where: {receipt_id:receipt_id}
+    })
+
+     if (getReceipt != null){
+        await prisma.receipts.delete({
+            where: {receipt_id}
+        })
+        res.status(200).json({success:true, message: 'Receipt deleted successfully.'})
+     } else{
+        res.status(500).json({success:false, message: 'Something went wrong. Server error'})
+     }
+   } catch (error) {
+    res.status(500).json({success:false, message:error.message})
+   }
 }
