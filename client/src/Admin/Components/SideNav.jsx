d99cd440a2c2c305d2d59e9581da9cac86618acc
Wilfred Muchire <logincoin40@gmail.com>
2024-09-16 14:34:54 +0300

    adding more changes to the design of the user interface

--- client/src/Admin/Components/SideNav.jsx
+++ client/src/Admin/Components/SideNav.jsx
@@ -1,44 +1,55 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './global.css'
-import Navigation from './Navigation';
+import { MdDashboardCustomize } from "react-icons/md";
+import { FaUsers } from "react-icons/fa6";
+import { IoSpeedometerSharp } from "react-icons/io5";
+import { LuFileSpreadsheet } from "react-icons/lu";
+import { FaRegRectangleList } from "react-icons/fa6";
 
 const SideNav = () => {
     return (
       <div>
         <div className="aside">
-          <nav>
-            <ul>
-              <li>
-                <Link className="li" to={"/dashboard"}>
-                  Dashboard
-                </Link>
-              </li>
-              <li>
-                <Link className="li" to={"/manage-customers"}>
-                  Customer management
-                </Link>
-              </li>
-              <li>
-                <Link className="li" to={"/manage-meters"}>
-                  Meter Management
-                </Link>
-              </li>
-              <li>
-                <Link className="li" to={"/billing-payment"}>
-                  Billing and Payments
-                </Link>
-              </li>
-              <li>
-                <Link className="li" to={"/manage-water-readings"}>
-                  Water readings
-                </Link>
-              </li>
-              <li>
-                <Link className="li">Settings</Link>
-              </li>
-            </ul>
-          </nav>
+          <div className="nav-links">
+            <div className="nav-1">
+              <Link className="li" to={"/dashboard"}>
+                <MdDashboardCustomize className="icons" />
+                Dashboard
+              </Link>
+            </div>
+
+            <div className="nav-1">
+              <Link className="li" to={"/manage-customers"}>
+                <FaUsers className="icons" />
+                Customers
+              </Link>
+            </div>
+
+            <div className="nav-1">
+              <Link className="li" to={"/manage-meters"}>
+                <IoSpeedometerSharp className="icons" />
+                Meters Management
+              </Link>
+            </div>
+
+            <div className="nav-1">
+              <Link className="li" to={"/billing-payment"}>
+                <LuFileSpreadsheet className="icons" />
+                Billing and Payments
+              </Link>
+            </div>
+
+            <div className="nav-1">
+              <Link className="li" to={"/manage-water-readings"}>
+                <FaRegRectangleList className="icons" />
+                Water readings
+              </Link>
+            </div>
+            <div className="nav-1">
+              <Link className="li">Settings</Link>
+            </div>
+          </div>
 
           <div className="logout">
             <button>Log out</button>
